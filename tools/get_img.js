0108fe6ec83faa46c99183e156e33d6bb643052d
ArezDev <arezzdev@gmail.com>
2025-09-01 09:28:49 +0700

    update getImg from local

--- tools/get_img.js
+++ tools/get_img.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
-
+const fs = require('fs');
+const path = require('path');
 /**
  * Downloads an image from the given URL and returns it as a base64 string with data URL prefix.
  * @returns {Promise<string>} Base64 encoded image with data URL prefix
  */
 async function getImageBase64() {
     const randomNum = Math.floor(Math.random() * 15) + 1;
+    // Use remote image, no local file read
     const url = `https://raw.githubusercontent.com/ArezDev/usa_new_img/refs/heads/main/a%20(${randomNum}).jpg`;
     const response = await axios.get(url, { responseType: 'arraybuffer' });
     const base64 = Buffer.from(response.data, 'binary').toString('base64');
@@ -22,4 +24,13 @@ async function getCoverImageBase64() {
     return `data:image/jpeg;base64,${base64}`;
 }
 
-module.exports = { getImageBase64, getCoverImageBase64 };
+async function getImageFromFIle() {
+    const randomNum = Math.floor(Math.random() * 101) + 1;
+    //const imagePath = `${path.dirname()}/img/a (${randomNum}).jpg`;
+    const imagePath = path.join(__dirname, '..', 'img', `a (${randomNum}).jpg`);
+    const imageBuffer = fs.readFileSync(imagePath);
+    const base64 = imageBuffer.toString('base64');
+    return `data:image/jpeg;base64,${base64}`;
+}
+
+module.exports = { getImageBase64, getCoverImageBase64, getImageFromFIle };
